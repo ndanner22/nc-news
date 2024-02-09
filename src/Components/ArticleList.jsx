025f2b9ae277c0493e29165f40fc58c09ce2672f
Neil Danner <ndanner22@gmail.com>
2024-02-09 16:03:58 +0000

    sort by dropdown menu added to all articles page - not yet functional

--- src/Components/ArticleList.jsx
+++ src/Components/ArticleList.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { getAllArticles } from "../Utils/api";
 import ArticleCard from "./ArticleCard";
 
 export default function ArticleList() {
   const [articlesData, setArticlesData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [sortCategory, setSortCategory] = useState("created_at");
 
   useEffect(() => {
     getAllArticles().then(({ data }) => {
@@ -16,6 +18,19 @@ export default function ArticleList() {
   return (
     <>
       <h2 className="article-list-header">Articles</h2>
+      <h3>Order Articles By: </h3>
+      <select
+        id="sortCategory"
+        value={sortCategory}
+        onChange={(event) => {
+          setSortCategory(event.target.value);
+        }}
+      >
+        <option value="created_at">Date</option>
+        <option value="comment_count">Comment Count</option>
+        <option value="votes">Votes</option>
+      </select>
+      <button>Reverse Order</button>
       <section className="article-list">
         {isLoading ? (
           <h2>Loading....</h2>
